0dcb8234c4b801d2f82c0bf5cd30ca32b06268c6
fadhil-khusnul <fadhilkh27@gmail.com>
2025-02-27 02:29:02 +0800

    Update Dashboard.jsx

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -17,26 +17,18 @@ export default function Dashboard({ semesterOptions, courseDetails, total_grafik
       // Tunda reload agar spinner terlihat oleh user
       setTimeout(() => {
         window.location.reload();
-      }, 150);
+      }, 1000);
     }
   }, [shouldRefresh]);
 
   return (
-    <AuthenticatedLayout
-      header={
-        <h2 className="text-xl font-semibold leading-tight text-gray-800 bg-gray-100 dark:text-gray-200">
-         {title}
-        </h2>
-      }
-    >
-      <Head title={title} />
-
+    <>
       <Backdrop
 
         sx={(theme) => ({
-          color: '#fff',
+          color: 'rgba(0, 0, 0, 0.9)',
           zIndex: theme.zIndex.drawer + 1,
-          backgroundColor: 'rgba(0, 0, 0, 0.9)',
+          backgroundColor: '#fff',
           backdropFilter: 'blur(4px)'
         })}
         open={open}
@@ -44,21 +36,34 @@ export default function Dashboard({ semesterOptions, courseDetails, total_grafik
         <CircularProgress color="inherit" />
       </Backdrop>
 
-      <div className="py-2">
-        <div className="mx-auto sm:px-6 lg:px-4">
-          <div className="grid gap-4 mx-2 lg:grid-cols-1">
-            <div className="w-full px-2 mb-4">
-              <FilterSelect semesterOptions={semesterOptions} filter={'statistik'} />
-            </div>
-            <div className="w-full px-2 mb-4">
-              {total_grafik ? <Grafik totals={total_grafik} /> : null}
-            </div>
-            <div className="w-full px-2 mb-4">
-              {courseDetails.data.length > 0 ? <Table courses={courseDetails} /> : null}
+      {!shouldRefresh && (
+        <AuthenticatedLayout
+          header={
+            <h2 className="text-xl font-semibold leading-tight text-gray-800 bg-gray-100 dark:text-gray-200">
+              {title}
+            </h2>
+          }
+        >
+          <Head title={title} />
+
+          <div className="py-2">
+            <div className="mx-auto sm:px-6 lg:px-4">
+              <div className="grid gap-4 mx-2 lg:grid-cols-1">
+                <div className="w-full px-2 mb-4">
+                  <FilterSelect semesterOptions={semesterOptions} filter={'statistik'} />
+                </div>
+                <div className="w-full px-2 mb-4">
+                  {total_grafik ? <Grafik totals={total_grafik} /> : null}
+                </div>
+                <div className="w-full px-2 mb-4">
+                  {courseDetails.data.length > 0 ? <Table courses={courseDetails} /> : null}
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-    </AuthenticatedLayout>
+        </AuthenticatedLayout>
+      )}
+
+    </>
   );
 }
